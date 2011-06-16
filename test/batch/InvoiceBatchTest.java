7779a10cd4a022ed2ba97c82105b28205779e962
Marvin Ede <marvin.ede@haw-hamburg.de>
2011-06-16 13:55:14 +0200

    added test for MQ. test prints every message that is send (consumer)

--- test/batch/InvoiceBatchTest.java
+++ test/batch/InvoiceBatchTest.java
@@ -2,9 +2,17 @@ package batch;
 
 import businessLogic.customerComponent.ICustomermanagement;
 import businessLogic.groupComponent.IGroupmanagement;
+import com.rabbitmq.client.ConsumerCancelledException;
+import java.io.IOException;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import play.test.UnitTest;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.QueueingConsumer;
+import java.util.Date;
 
 /**
  *
@@ -21,10 +29,32 @@ public class InvoiceBatchTest extends UnitTest {
     customermanagement = buildAndConfigure.BuildAndConfigureSystem.getCustomerComponent();
     groupmanagement = buildAndConfigure.BuildAndConfigureSystem.getGroupComponent();
   }
-  
+
   @Test
-  public void runBatch() {
+  public void runBatch() throws IOException, ConsumerCancelledException, InterruptedException {
     InvoiceBatch invoiceBatch = new InvoiceBatch();
     invoiceBatch.doJob();
+    Consumer consumer = new Consumer();
+    consumer.readMessages();
+  }
+
+  private class Consumer extends MessageProxy{
+
+
+    void readMessages() throws IOException, ConsumerCancelledException, InterruptedException{
+      if (establishConnection()) {
+        QueueingConsumer consumer = new QueueingConsumer(channel);
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+
+        while (true) {
+          QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+          String message = new String(delivery.getBody());
+          System.out.println("Message has been send at "+new Date()+":\n"+message+"\n");
+        }
+        
+      }
+      closeConnection();
+      
+    }
   }
 }
